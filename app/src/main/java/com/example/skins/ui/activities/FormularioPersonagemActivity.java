0442c572127fcfe0b19b92bef26248875705ea70
Aluno <aluno@LI.LOCAL>
2022-04-01 21:23:05 -0300

    ai que preguiça de ninfeta

--- app/src/main/java/com/example/skins/ui/activities/FormularioPersonagemActivity.java
+++ app/src/main/java/com/example/skins/ui/activities/FormularioPersonagemActivity.java
@@ -1,4 +1,61 @@
 package com.example.skins.ui.activities;
 
-public class FormularioPersonagemActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+
+import com.example.skins.R;
+import com.example.skins.dao.PersonagemDAO;
+import com.example.skins.model.Personagem;
+
+public class FormularioPersonagemActivity extends AppCompatActivity {
+
+    private static final String TITULO_APPBAR_EDITA_PERSONAGEM = "Editar o Personagem";
+    private static final String TITULO_APPBAR_NOVA_PERSONAGEM = "Novo Personagem";
+    private EditText campoNome;
+    private EditText campoNascimento;
+    private EditText campoAltura;
+    private final PersonagemDAO dao = new PersonagemDAO();
+    private Personagem personagem;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.activity_formulario_personagem_menu_salvar, menu);
+        return super.onCreateOptionsMenu(menu);
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        int itemId = item.getItemId();
+        if (itemId ==  R.id.activity_formulario_personagem_menu_salvar){
+            finalizarFormulario();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_personagem);
+        inicializacaoCampos();
+        //configuraBotaoSalvar();
+        carregaPersonagem();
+        checaPermissoes();
+
+    }
+    private  void carregaPersonagem(){
+        Intent dados  = getIntent();
+        if (dados.hasExtra(CHAVE_PERSONAGEM)){
+            setTitle(TITULO_APPBAR_EDITA_PERSONAGEM);
+            personagem = (Personagem) dados.getSerializableExtra(CHAVE_PERSONAGEM);
+            preecheCampos();
+        }
+
+    }
+
 }
