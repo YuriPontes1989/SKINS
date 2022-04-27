d0279fa4b47c46d99a1604108a62416c3e871d41
BloodyValentine <yuri.cozendei1999@hotmail.com>
2022-04-27 17:08:59 -0300

    :(

--- app/src/main/java/com/example/skins/dao/PersonagemDAO.java
+++ app/src/main/java/com/example/skins/dao/PersonagemDAO.java
@@ -1,4 +1,46 @@
 package com.example.skins.dao;
 
+import com.example.skins.model.Personagem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonagemDAO {
+    private final static List<Personagem> personagens = new ArrayList<>();
+    private static int contadorDeIds = 1;
+
+    public  void salvar(Personagem persoangemSalvo) {
+        persoangemSalvo.setId(contadorDeIds);
+        personagens.add(persoangemSalvo);
+        atualizaId();
+    }
+
+    private void atualizaId() {contadorDeIds++; }
+
+    public void edita(Personagem personagem) {
+        Personagem personagemEncontrado = buscaPersonagemId(personagem);
+        if (personagemEncontrado != null) {
+            int posicaoDoPersonagem = personagens.indexOf(personagemEncontrado);
+            personagens.set(posicaoDoPersonagem, personagem);
+        }
+    }
+
+    private Personagem buscaPersonagemId(Personagem personagem) {
+        for (Personagem p :
+                personagens) {
+            if (p.getId() == personagem.getId()) {
+                return p;
+            }
+        }
+        return  null;
+    }
+
+    public List<Personagem> todos() {return  new ArrayList<>(personagens);}
+
+    public void remove(Personagem personagem) {
+        Personagem personagemDevolvido = buscaPersonagemId((personagem));
+        if (personagemDevolvido != null) {
+            personagens.remove(personagemDevolvido);
+        }
+    }
 }
